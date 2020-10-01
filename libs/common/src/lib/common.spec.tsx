1db6005c131ea79ac682af6ec431e0fe37961283
MilenaMMay <milenammay@googlemail.com>
2020-10-01 13:29:50 +0200

    add common lib

---
+++ libs/common/src/lib/common.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Common from './common';
+
+describe('Common', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Common />);
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ libs/common/src/lib/common.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './common.css';
+
+/* eslint-disable-next-line */
+export interface CommonProps {}
+
+export const Common = (props: CommonProps) => {
+  return (
+    <div>
+      <h1>Welcome to common!</h1>
+    </div>
+  );
+};
+
+export default Common;
