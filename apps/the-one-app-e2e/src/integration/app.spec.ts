9ad449c02681ef1413efba13756e95390b169f0f
MilenaMMay <milenammay@googlemail.com>
2020-10-28 11:42:30 +0100

    fix expectation for the one app e2e

--- apps/the-one-app-e2e/src/integration/app.spec.ts
+++ apps/the-one-app-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('the-one-app', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to the-one-app!');
+    getGreeting().contains('I AM THE ONE APP');
   });
 });
